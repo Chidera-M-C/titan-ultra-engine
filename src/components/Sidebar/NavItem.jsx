5c40adae9c6d374a95dea3a9025c319d327aae95
Chidera-M-C <mmandu2022@gmail.com>
2026-02-11 15:39:39 +0100

    Update NavItem.jsx

--- src/components/Sidebar/NavItem.jsx
+++ src/components/Sidebar/NavItem.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+export default function NavItem({ icon: Icon, label, isActive, onClick }) {
+  return (
+    <button 
+      className={isActive ? 'active' : ''} 
+      onClick={onClick}
+    >
+      <Icon size={20} /> 
+      <span>{label}</span>
+    </button>
+  );
+}
