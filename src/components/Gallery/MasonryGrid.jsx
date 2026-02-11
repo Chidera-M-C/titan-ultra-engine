5d2d528ea5b0c7aa69811e68d0d4275f34217e96
Chidera-M-C <mmandu2022@gmail.com>
2026-02-11 23:53:45 +0100

    Update MasonryGrid.jsx

--- src/components/Gallery/MasonryGrid.jsx
+++ src/components/Gallery/MasonryGrid.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import ImageCard from './ImageCard';
 
+export default function MasonryGrid({ images, onImageClick, actionLabel }) {
+  return (
+    <div className="masonry-grid">
+      {images.map((img) => (
+        <ImageCard 
+          key={img.id}
+          url={img.url}
+          prompt={img.prompt}
+          onAction={onImageClick}
+          actionLabel={actionLabel}
+        />
+      ))}
+    </div>
+  );
+}
