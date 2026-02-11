2b64bba9eaa0ab131a217ac38dd9136f32b94613
Chidera-M-C <mmandu2022@gmail.com>
2026-02-11 23:53:01 +0100

    Update ImageCard.jsx

--- src/components/Gallery/ImageCard.jsx
+++ src/components/Gallery/ImageCard.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+export default function ImageCard({ url, prompt, onAction, actionLabel = "Remix" }) {
+  return (
+    <div className="pin-item" onClick={() => onAction(prompt)}>
+      <img src={url} alt="Artwork" loading="lazy" />
+      <div className="pin-overlay">
+        <button className="use-btn">{actionLabel}</button>
+      </div>
+    </div>
+  );
+}
