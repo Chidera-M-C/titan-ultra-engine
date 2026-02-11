f7771d3848418319c32324eb9349409523dbbd4b
Chidera-M-C <mmandu2022@gmail.com>
2026-02-11 23:57:04 +0100

    Update CharacterView.jsx

--- src/views/CharacterView.jsx
+++ src/views/CharacterView.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import EmptyState from '../components/Shared/EmptyState';
+import { User } from 'lucide-react';
 
+export default function CharacterView() {
+  return (
+    <EmptyState 
+      icon={User} 
+      title="Character Lab" 
+      description="Consistent character generation is coming soon. Stay tuned!" 
+    />
+  );
+}
