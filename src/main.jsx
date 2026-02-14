a95ee1876b156c692af7a4b4ae6df5918a6e384c
Chidera-M-C <mmandu2022@gmail.com>
2026-02-14 01:29:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// 1. Import the provider you just created
+import { AuthProvider } from './context/AuthContext' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap your App with the AuthProvider */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
