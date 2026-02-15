1f4f3c7494311e895b2cf330634d50c124714b28
Chidera-M-C <mmandu2022@gmail.com>
2026-02-15 13:51:34 +0100

    Create ImageViewModal.jsx

---
+++ src/components/Shared/ImageViewModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageViewModal.css';
+
+export default function ImageViewModal({ imageUrl, onClose }) {
+  return (
+    <div className="image-view-modal" onClick={onClose}>
+      <div className="image-view-content" onClick={(e) => e.stopPropagation()}>
+        <img 
+          src={imageUrl} 
+          alt="Full view" 
+          className="image-view-img"
+        />
+      </div>
+      
+      <button className="image-view-close" onClick={onClose} data-tooltip="Close">
+        ✕
+      </button>
+    </div>
+  );
+}
