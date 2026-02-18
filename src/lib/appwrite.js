ceb5d4e158f52db5e9e70db9481d12534231ff19
Chidera-M-C <mmandu2022@gmail.com>
2026-02-18 01:24:31 +0100

    Update appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,15 +1,12 @@
 import { Client, Account, Databases, Storage, ID, Query, Functions } from 'appwrite';
 
 const client = new Client()
-    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+    .setEndpoint('https://cloud.appwrite.io/v1') 
+    .setProject('YOUR_ACTUAL_PROJECT_ID_HERE'); // PASTE YOUR PROJECT ID DIRECTLY HERE
 
 export const account = new Account(client);
 export const storage = new Storage(client);
 export const db = new Databases(client);
+export const functions = new Functions(client);
 
-// Add this so you can use it in your CreditsCard
-export const functions = new Functions(client); 
-
-// FIX: Export the 'client' you created above, and the helpers from the library
 export { client, ID, Query };
