bf1d7eeae31d91b0c591e99511e2159f4d22beaf
Chidera-M-C <mmandu2022@gmail.com>
2026-02-19 03:00:12 +0100

    Update ImageViewModal.jsx

--- src/components/Shared/ImageViewModal.jsx
+++ src/components/Shared/ImageViewModal.jsx
@@ -3,20 +3,11 @@ import './ImageViewModal.css';
 
 export default function ImageViewModal({ imageUrl, onClose }) {
   return (
-    /* Added modal-overlay so App.jsx allows clicking the background to close */
-    <div className="image-view-modal modal-overlay" onClick={onClose}>
+    <div className="image-view-modal" onClick={onClose}>
       <div className="image-view-content" onClick={(e) => e.stopPropagation()}>
-        <img 
-          src={imageUrl} 
-          alt="Full view" 
-          className="image-view-img"
-        />
+        <img src={imageUrl} alt="Full view" className="image-view-img" />
+        <button className="image-view-close" onClick={onClose}>✕</button>
       </div>
-      
-      {/* Added close-button so App.jsx allows clicking the X to close */}
-      <button className="image-view-close close-button" onClick={onClose} data-tooltip="Close">
-        ✕
-      </button>
     </div>
   );
 }
