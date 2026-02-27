e8c250cab39059f9ad103cbc0e252b3bc3827aa6
Chidera-M-C <mmandu2022@gmail.com>
2026-02-27 06:36:50 -0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These come from your Supabase Dashboard -> Settings -> API
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.https://rtklziobobnsqxsozmoq.supabase.co;
+const supabaseKey = import.meta.env.sb_publishable_-RmylFTABAYLkyk9s4qK0g_n-WGxHq3;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
