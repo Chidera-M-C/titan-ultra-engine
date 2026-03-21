dae06705d966954965e578477610f201ea81b216
Chidera-M-C <mmandu2022@gmail.com>
2026-03-21 03:33:11 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <App />
