6f34f280b77f021431421068ede52e657dc012b1
Chidera-M-C <mmandu2022@gmail.com>
2026-03-23 02:52:59 +0100

    Add endpoint for face embedding in generate.js

--- functions/api/generate.js
+++ functions/api/generate.js
@@ -27,6 +27,7 @@ export async function onRequestPost(context) {
     if (style && BIGLUST_STYLES.has(style))      endpointId = env.RUNPOD_ENDPOINT_BIGLUST;
     if (style && EDIT_STYLES.has(style)) endpointId = env.RUNPOD_ENDPOINT_EDIT;
     if (style && style === 'character') endpointId = env.RUNPOD_ENDPOINT_CHARACTER;
+    if (face_embedding) endpointId = env.RUNPOD_ENDPOINT_CHARACTER;
 
     const controller = new AbortController();
     const timeout = setTimeout(() => controller.abort(), 25000);
