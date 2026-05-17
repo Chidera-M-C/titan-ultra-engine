0f0f8a74a3b35ca5385a1759088b1dd652eb59f8
Chidera-M-C <mmandu2022@gmail.com>
2026-05-17 14:13:30 +0100

    Update [[path]].ts

--- functions/api/auth/[[path]].ts
+++ functions/api/auth/[[path]].ts
@@ -1,5 +1,7 @@
 import { auth } from "../../../src/lib/auth";
 
 export const onRequest = async (context: any) => {
+  // Inject Cloudflare env into process.env so auth.ts can read them
+  Object.assign(process.env, context.env);
   return auth.handler(context.request);
 };
