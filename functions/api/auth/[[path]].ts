bccaa8b390cfdcd8024059e210aefe57f8ab1124
Chidera-M-C <mmandu2022@gmail.com>
2026-05-17 04:46:58 +0100

    Update and rename functions/api/auth.ts to functions/api/auth/[[path]].ts

--- functions/api/auth/[[path]].ts
+++ functions/api/auth/[[path]].ts
@@ -1,4 +1,4 @@
-import { auth } from "../../src/lib/auth";
+import { auth } from "../../../src/lib/auth";
 
 export const onRequest = async (context: any) => {
   return auth.handler(context.request);
