293bd49c77944e1fe0ee395393fc43b519a70f9e
Chidera-M-C <mmandu2022@gmail.com>
2026-05-17 04:05:31 +0100

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,6 +3,7 @@ import { createAuthClient } from 'better-auth/client';
 
 const AuthContext = createContext();
 
+
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [credits, setCredits] = useState(0);
