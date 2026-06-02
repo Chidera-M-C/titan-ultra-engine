716f5ad4225dcd93d8f41ee8a9d61acf1b5c010f
Chidera-M-C <mmandu2022@gmail.com>
2026-06-02 23:33:02 +0100

    Update ImageToVideoView.jsx

--- src/views/ImageToVideoView.jsx
+++ src/views/ImageToVideoView.jsx
@@ -55,6 +55,7 @@ function CharacterPicker({ characters, selectedCharacter, onSelect, onClose, onC
           onCreated={(c) => { if (onCharacterCreated) onCharacterCreated(c); setShowCreate(false); onClose(); }} />
       )}
     </>
+    document.body
   );
 }
 
