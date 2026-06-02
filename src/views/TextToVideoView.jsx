56d19ec5ff933cd7d38c37529ed407ddb36b8779
Chidera-M-C <mmandu2022@gmail.com>
2026-06-02 23:35:37 +0100

    Fix JSX syntax in TextToVideoView component

--- src/views/TextToVideoView.jsx
+++ src/views/TextToVideoView.jsx
@@ -53,7 +53,7 @@ function CharacterPicker({ characters, selectedCharacter, onSelect, onClose, onC
         <CreateCharacterModal onClose={() => setShowCreate(false)}
           onCreated={(c) => { if (onCharacterCreated) onCharacterCreated(c); setShowCreate(false); onClose(); }} />
       )}
-    </>
+    </>,
     document.body
   );
 }
