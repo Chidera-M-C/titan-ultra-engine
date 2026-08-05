0c6b670ce75211d5d249a4448c7e9909f2ee2874
Chidera-M-C <mmandu2022@gmail.com>
2026-08-05 08:36:16 +0100

    Update download URL for Juggernaut XL model

--- handlers/download_models_edit.py
+++ handlers/download_models_edit.py
@@ -28,7 +28,7 @@ def download_civitai(url, dest_path, label):
 
 # 1. Base Checkpoint & VAE
 download_civitai(
-    "https://civitai.com/api/download/models/1759168?type=Model&format=SafeTensor&size=full&fp=fp16",
+    "https://civitai.red/api/download/models/1081768?fileId=986850",
     "/workspace/juggernaut_xl.safetensors",
     "Juggernaut XL"
 )
