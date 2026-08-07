3a454e17c01b85eb16cbdc2bd35eb98c1dafed1b
Chidera-M-C <mmandu2022@gmail.com>
2026-08-07 21:33:36 +0100

    Update download_models_edit.py

--- handlers/download_models_edit.py
+++ handlers/download_models_edit.py
@@ -28,7 +28,7 @@ def download_civitai(url, dest_path, label):
 
 # 1. Base Checkpoint & VAE
 download_civitai(
-    "https://civitai.red/api/download/models/2551619?fileId=2440003",
+    "https://civitai.red/api/download/models/2574712?fileId=2462118",
     "/workspace/juggernaut_xl.safetensors",
     "Juggernaut XL"
 )
