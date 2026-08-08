e7720e68211fb5f5aa5ecfcb48e8cbc30474e572
Chidera-M-C <mmandu2022@gmail.com>
2026-08-08 11:01:58 +0100

    Update comments and organize download sections

--- handlers/download_models_edit.py
+++ handlers/download_models_edit.py
@@ -40,7 +40,7 @@ hf_hub_download(
     token=HF_TOKEN or None
 )
 
-# 2. New Detail Tweaker LoRA (replaces old one)
+# 2. Detail Tweaker LoRA
 download_civitai(
     "https://civitai.red/api/download/models/135867?fileId=99264",
     "/workspace/detail_tweaker.safetensors",
@@ -54,14 +54,7 @@ download_civitai(
     "Realism LoRA"
 )
 
-# 4. NSFW All-In-One LoRA
-download_civitai(
-    "https://civitai.red/api/download/models/160240?fileId=120561",
-    "/workspace/nsfw_allinone.safetensors",
-    "NSFW All-In-One LoRA"
-)
-
-# 5. ControlNets
+# 4. ControlNets
 snapshot_download(
     repo_id="xinsir/controlnet-openpose-sdxl-1.0",
     local_dir="/workspace/controlnet_openpose_xl",
@@ -76,7 +69,7 @@ snapshot_download(
     token=HF_TOKEN or None
 )
 
-# 6. IP-Adapter FaceID Plus v2
+# 5. IP-Adapter FaceID Plus v2
 hf_hub_download(
     repo_id="h94/IP-Adapter-FaceID",
     filename="ip-adapter-faceid-plusv2_sdxl.bin",
@@ -91,14 +84,14 @@ hf_hub_download(
     token=HF_TOKEN or None
 )
 
-# 7. CLIP Image Encoder
+# 6. CLIP Image Encoder
 snapshot_download(
     repo_id="laion/CLIP-ViT-H-14-laion2B-s32B-b79K",
     local_dir="/workspace/image_encoder",
     token=HF_TOKEN or None
 )
 
-# 8. OpenPose Pre-cache
+# 7. OpenPose Pre-cache
 from controlnet_aux import OpenposeDetector
 OpenposeDetector.from_pretrained("lllyasviel/ControlNet")
 
