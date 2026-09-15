4f8f192952fba5f0ffa87eb37cb00355189024bb
Chidera-M-C <mmandu2022@gmail.com>
2026-09-16 00:18:52 +0100

    Add new LoRA model download link

--- handlers/download_models_video.py
+++ handlers/download_models_video.py
@@ -56,6 +56,7 @@ OPENCLIP_CANDIDATES = [
 
 # Style LoRAs + proper dual Wan 2.2 Lightning LoRAs
 LORAS = [
+    ("https://civarchive.com/api/download/models/2391829", f"{LORAS_DIR}/lora_undress.safetensors", "LoRA: Undress"),
     ("https://civarchive.com/api/download/models/2329863", f"{LORAS_DIR}/lora_missionary.safetensors", "LoRA: Missionary"),
     ("https://civarchive.com/api/download/models/2200389", f"{LORAS_DIR}/lora_doggy.safetensors", "LoRA: Doggy"),
     ("https://civarchive.com/api/download/models/2121297", f"{LORAS_DIR}/lora_blowjob.safetensors", "LoRA: Blowjob"),
