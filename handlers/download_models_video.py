fd515e3f9b30c6049cb3b96ce10ffa054f3e5477
Chidera-M-C <mmandu2022@gmail.com>
2026-09-16 00:15:01 +0100

    Update LoRA URLs in download_models_video.py

--- handlers/download_models_video.py
+++ handlers/download_models_video.py
@@ -56,9 +56,9 @@ OPENCLIP_CANDIDATES = [
 
 # Style LoRAs + proper dual Wan 2.2 Lightning LoRAs
 LORAS = [
-    ("https://civitaiarchive.com/api/download/models/2496698", f"{LORAS_DIR}/lora_missionary.safetensors", "LoRA: Missionary"),
-    ("https://civitaiarchive.com/api/download/models/2513548", f"{LORAS_DIR}/lora_doggy.safetensors", "LoRA: Doggy"),
-    ("https://civitaiarchive.com/api/download/models/2446660", f"{LORAS_DIR}/lora_blowjob.safetensors", "LoRA: Blowjob"),
+    ("https://civarchive.com/api/download/models/2329863", f"{LORAS_DIR}/lora_missionary.safetensors", "LoRA: Missionary"),
+    ("https://civarchive.com/api/download/models/2200389", f"{LORAS_DIR}/lora_doggy.safetensors", "LoRA: Doggy"),
+    ("https://civarchive.com/api/download/models/2121297", f"{LORAS_DIR}/lora_blowjob.safetensors", "LoRA: Blowjob"),
     ("https://civitaiarchive.com/api/download/models/2508339", f"{LORAS_DIR}/lora_facial_cumshot.safetensors", "LoRA: Facial Cumshot"),
 
     # Proper dual Lightning for Wan 2.2
