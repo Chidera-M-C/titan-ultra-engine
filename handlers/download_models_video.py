3407b12e3753b60c0022ee18c688b69dc2ec4ed6
Chidera-M-C <mmandu2022@gmail.com>
2026-09-16 11:05:56 +0100

    Update LoRA URLs in download_models_video.py

--- handlers/download_models_video.py
+++ handlers/download_models_video.py
@@ -56,11 +56,11 @@ OPENCLIP_CANDIDATES = [
 
 # Style LoRAs + proper dual Wan 2.2 Lightning LoRAs
 LORAS = [
-    ("https://civarchive.com/api/download/models/2005946", f"{LORAS_DIR}/lora_undress.safetensors", "LoRA: Undress"),
+    ("https://civitai.red/api/download/models/2391828?fileId=2282070", f"{LORAS_DIR}/lora_undress.safetensors", "LoRA: Undress"),
     ("https://civitai.red/api/download/models/2245426?fileId=2137648", f"{LORAS_DIR}/lora_missionary.safetensors", "LoRA: Missionary"),
-    ("https://civitai.red/api/download/models/2200389?fileId=2093380", f"{LORAS_DIR}/lora_doggy.safetensors", "LoRA: Doggy"),
-    ("https://civitai.red/api/download/models/2121297?fileId=2017461", f"{LORAS_DIR}/lora_blowjob.safetensors", "LoRA: Blowjob"),
-    ("https://civitaiarchive.com/api/download/models/2508339", f"{LORAS_DIR}/lora_facial_cumshot.safetensors", "LoRA: Facial Cumshot"),
+    ("https://civitaiarchive.com/api/download/models/2513563", f"{LORAS_DIR}/lora_doggy.safetensors", "LoRA: Doggy"),
+    ("https://civitaiarchive.com/api/download/models/2446660", f"{LORAS_DIR}/lora_blowjob.safetensors", "LoRA: Blowjob"),
+    ("https://civitaiarchive.com/api/download/models/2508356", f"{LORAS_DIR}/lora_facial_cumshot.safetensors", "LoRA: Facial Cumshot"),
 
     # Proper dual Lightning for Wan 2.2
     (
